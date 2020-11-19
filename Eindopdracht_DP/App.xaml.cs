1b201f04162db86a5c874200bb6a292d85d00b56
Margot <margot.de.haene@student.howest.be>
2020-11-19 16:58:13 +0100

    Setup project

---
+++ Eindopdracht_DP/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Eindopdracht_DP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            VersionTracking.Track();
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Eindopdracht_DP/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Eindopdracht_DP.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Eindopdracht_DP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (VersionTracking.IsFirstLaunchEver)
+            {
+                Navigation.PushModalAsync(new OnboardingPage());
+            }
+        }
+    }
+}
